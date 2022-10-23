3dc68fda406fe05e9b705d0e2fb538bf18f4b700
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-10-23 12:50:26 +0100

    Dates and Time Intro

--- DatesAndTimes/DatesAndTimes/Program.cs
+++ DatesAndTimes/DatesAndTimes/Program.cs
@@ -6,7 +6,29 @@ namespace DatesAndTimes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(-3).ToLongTimeString());
+
+            //Console.WriteLine(myValue.Month);
+
+
+            //DateTime myBirthday = new DateTime(1969, 12, 7);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("12/6/1988");
+
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+
+            //Console.WriteLine(myValue);
+            Console.ReadLine();
         }
     }
 }
